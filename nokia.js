b31c0ebb407157e74eb3efbe0aaf7552e25b91e3
runningtalus <dave@davemay.org>
2014-05-01 23:03:02 -0600

    jQuery implementation for map.set baseMapType completed.  Nokia Here would not accept DOT notation.

--- nokia.js
+++ nokia.js
@@ -2,16 +2,19 @@ $(document).on('ready', function(){
 
 	// nokia.Settings.set("app_id", "YOUR_APPID");
 	nokia.Settings.set('app_id', '');
-  // nokia.Settings.set("app_code", "YOUR_TOKEN");
+	// nokia.Settings.set("app_code", "YOUR_TOKEN");
 	nokia.Settings.set('app_code', '');
 
 	var map = new nokia.maps.map.Display(document.getElementById('nokia'), {
-		// Zoom level for the map
-		zoomLevel: 10,
-		// Map center coordinates
+		
+		baseMapType: nokia.maps.map.Display.SATELLITE,
 		center: [40.0079, -105.2348],
-		// Activate satellite imagery on the display:
-    baseMapType: nokia.maps.map.Display.SATELLITE
+		zoomLevel: 10
+	});
+
+	$('.maptype').on('click', function(){
+		var mapType = $(this).attr('data-maptype'); 
+		map.set("baseMapType", map[mapType]);  //HERE does NOT accept DOT notation
 	});
 
 });
