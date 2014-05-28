4d85740b55b2bb724f2a5f416777814d56f52f45
runningtalus <dave@davemay.org>
2014-05-28 12:51:42 -0600

    Checkboxes created.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
 var app = express();
 app.set('view engine', 'jade');
@@ -8,9 +7,22 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
-app.get('/', function(req, res) {
+app.all('/', function(req, res) {
+	// normalize map value as an array
+	// req.body.googleMap = [].concat(req.body.googleMap || []);
+	// req.body.nokiaMap = [].concat(req.body.nokiaMap || []);
+
+	req.query.bing = [].concat(req.query.bing || []);
+	req.query.esri = [].concat(req.query.esri || []);
+	req.query.google = [].concat(req.query.google || []);
+	req.query.mapquest = [].concat(req.query.mapquest || []);
+	req.query.nokia = [].concat(req.query.nokia || []);
+
+	// console.log('req.body', req.body);
+	console.log('req.query', req.query);
+
 	res.render('index');
-});
+	});
 
 var server = app.listen(7511, function() {
 	console.log('Express server listening on port ' + server.address().port);
